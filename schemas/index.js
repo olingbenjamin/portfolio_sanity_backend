b1c2971b8d591b86e28c22b337167cf20955a6ae
Oling Emmanuel Benjamin <olingemmanuel5@gmail.com>
2023-09-07 16:05:52 +0300

    sanity first commit

--- schemas/index.js
+++ schemas/index.js
@@ -1,1 +1,11 @@
-export const schemaTypes = []
+import abouts from "./abouts"
+import testimonials from "./testimonials"
+import brands from "./brands";
+import contact from './contact';
+import experiences from './experiences';
+import skills from './skills';
+import works from './works';
+import workExperience from './workExperience';
+
+
+export const schemaTypes = [testimonials,abouts,brands,contact,experiences,skills,works,workExperience]
